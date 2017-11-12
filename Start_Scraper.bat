77cb900476bd793caa321e2e3758b5db6e794d42
Remmy14 <33599081+Remmy14@users.noreply.github.com>
2017-11-12 09:18:14 -0500

    Initial commit

---
+++ Start_Scraper.bat
@@ -0,0 +1,3 @@
+@echo off
+python C:\Users\Remillards\PycharmProjects\CraigslistScraper\scraper.py
+pause
---
+++ scraper.py
@@ -0,0 +1,112 @@
+import requests
+from bs4 import BeautifulSoup as bs4
+import logging
+import gmail
+import time
+from random import randint
+import sys
+
+postings = []
+FORMAT = '%(asctime)s.%(msecs)03d [%(process)d] [%(levelname)s] - %(message)s'
+logging.basicConfig(format=FORMAT,
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='scraper.log',
+                    filemode='a+',
+                    level=10)
+
+logging.debug("Scraper started.")
+
+def get_price(text):
+    prices = text.find_all('span', attrs={'class':'result-price'})
+    if len(prices) > 0:
+        return prices[0].get_text()
+    else:
+        logging.critical("Failed to get a price")
+        return 'None'
+
+def get_location(text):
+    locations = text.find_all('span', attrs={'class':'result-hood'})
+    if len(locations) == 0:
+        locations = text.find_all('span', attrs={'class':'nearby'})
+
+    if len(locations) == 0:
+        # We tried our best. Let's get the fuck outta dodge
+        logging.critical("Failed to get a location")
+        return 'None.'
+    else:
+        return locations[0].get_text()
+
+def get_post_name(text):
+    names = text.find_all('a', attrs={'class':'result-title hdrlnk'})
+    try:
+        return names[0].get_text()
+    except:
+        logging.critical("Failed to get a name")
+        return None
+
+def get_post_link(text):
+    try:
+        return text.a['href']
+    except:
+        logging.critical("Failed to get a link")
+        return None
+
+def get_post_time(text):
+    try:
+        return text.time['title']
+    except:
+        logging.critical("Failed to get a post time")
+        return None
+
+while True:
+    city = "dayton"
+    url = ".craigslist.org/search/sss/?query="
+
+    thing_list_file = open('item_list_file.txt', 'r')
+    thing_list = thing_list_file.readlines()
+    thing_list_file.close()
+
+    for thing in thing_list:
+        query = "https://" + city + url + thing.strip()
+
+        rsp = requests.get(query)
+        html = bs4(rsp.text, 'html.parser')
+        new_items = html.find_all('li', attrs={'class':'result-row'})
+        number_of_items = len(new_items)
+        number_of_new_items = 0
+
+        for item in new_items:
+            pid = item['data-pid']
+            if pid not in postings:
+                # We have a unique new listing and need to email out
+                number_of_new_items += 1
+                price = get_price(item)
+                local = get_location(item)
+                name = get_post_name(item)
+                link = get_post_link(item)
+                post_time = get_post_time(item)
+
+                body = "Name: {0}\nPrice: {1}\nLocation: {2}\nPosted: {3}\nLink: {4}".format(name, price, local, post_time, link)
+
+                # Read in uname/pword here so that it's not in memory
+                creds_file = open('arcf/.creds_file.txt', 'r')
+                uname = creds_file.readline().strip()
+                pword = creds_file.readline().strip()
+                creds_file.close()
+                gm = gmail.GMail(uname, pword)
+                uname = None
+                pword = None
+                gm.connect()
+
+                try:
+                    msg = gmail.Message('New Humidor Found on Craigslist', to = 'alex.remillard@gmail.com', text=body)
+                    gm.send(msg)
+                    postings.append(pid)
+                except:
+                    logging.debug("Something went wrong for posting {}".format(pid))
+                    pass
+
+            logging.info("For item {0} we found {1} items, {2} of which are new".format(thing, number_of_items, number_of_new_items))
+
+            # Sleep for between 3 and 5 minutes
+            time.sleep(randint(180, 300))
